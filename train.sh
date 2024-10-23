08dcaed3d7186944b8d3aabc52f526fa9da7c564
phuonglinh81 <akagamishirayuki88@gmail.com.com>
2024-10-23 07:58:09 +0000

    Add train.sh

---
+++ train.sh
@@ -0,0 +1,4 @@
+python speech_to_text_ctc_bpe.py\
+    --config-path="/content/Fast-conformer-NVIDIA-Vivos/vocab_tokenizers/tokenizer_spe_bpe_v10000" \
+    --config-name="fast-conformer_ctc_bpe"
+    --log_dir="/content/runs"
