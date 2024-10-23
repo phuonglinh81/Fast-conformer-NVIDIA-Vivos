7018d1aeee2e907e2fb97da92110c18ede4830a1
phuonglinh81 <akagamishirayuki88@gmail.com.com>
2024-10-23 14:26:06 +0000

    Update import & function

--- speech_to_text_ctc_bpe.py
+++ speech_to_text_ctc_bpe.py
@@ -66,25 +66,24 @@ https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/main/asr/results.ht
 
 import pytorch_lightning as pl
 from omegaconf import OmegaConf
-
+import nemo.collections.asr as nemo_asr
 from nemo.collections.asr.models.ctc_bpe_models import EncDecCTCModelBPE
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
-from nemo.utils.trainer_utils import resolve_trainer_cfg
 
 
-@hydra_runner(config_path="../conf/citrinet/", config_name="config_bpe")
+@hydra_runner(config_path="../conf/citrinet/", config_name="fast-conformer_ctc_bpe")
 def main(cfg):
     logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
 
-    trainer = pl.Trainer(**resolve_trainer_cfg(cfg.trainer))
+    trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
     asr_model = EncDecCTCModelBPE(cfg=cfg.model, trainer=trainer)
 
     # Initialize the weights of the model from another model, if provided via config
     asr_model.maybe_init_from_pretrained_checkpoint(cfg)
-
+    # asr_model.encoder.freeze()
     trainer.fit(asr_model)
 
     if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.manifest_filepath is not None:
