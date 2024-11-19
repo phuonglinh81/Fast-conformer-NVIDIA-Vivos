e15f5adeb431bef0d7c1fee6ab042e6fe809ac3f
phuonglinh81 <akagamishirayuki88@gmail.com>
2024-11-19 15:25:42 +0000

    Update axis

--- KenLM/eval_beamsearch_ngram_ctc.py
+++ KenLM/eval_beamsearch_ngram_ctc.py
@@ -334,7 +334,7 @@ def main(cfg: EvalBeamSearchNGramConfig):
     for batch_idx, probs in enumerate(all_probs):
         print(probs)
         print(type(probs))
-        preds = np.argmax(probs, axis=1)
+        preds = np.argmax(probs, axis=2)
         preds_tensor = torch.tensor(preds, device='cpu').unsqueeze(0)
         if isinstance(asr_model, EncDecHybridRNNTCTCModel):
             pred_text = asr_model.ctc_decoding.ctc_decoder_predictions_tensor(preds_tensor)[0][0]
